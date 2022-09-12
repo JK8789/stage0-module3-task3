bf27195fe03db9a72b6ffe2e839df96cc91e2e8e
Korotkova Julia <korotkovayuliav@gmail.com>
2022-09-12 17:41:42 +0200

    task 3 is done

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,11 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int firstNumber = 1;
+        int secondNumber = 10;
+        int thirdNumber = 100;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,23 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int firstNumber = 1;
+        int secondNumber = 10;
+        int thirdNumber = 100;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
+        int linkToFirst = firstNumber;
+        int linkToSecond = secondNumber;
+        int linkToThird = thirdNumber;
+        firstNumber = 15;
+        secondNumber = 6;
+        thirdNumber = 4;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
